63cd6f48e4ffbd6e6dcde0c3ba89abcd4f45c0ea
Vahid <vahid.hedayati@gmail.com>
2014-08-26 04:41:36 +0100

    0.10 release

--- WschatGrailsPlugin.groovy
+++ WschatGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class WschatGrailsPlugin {
-    def version = "0.9"
+    def version = "0.10"
     def grailsVersion = "2.0 > *"
     def title = "Websocket Chat Plugin"
     def description = 'default websocket chat room plugin.'
