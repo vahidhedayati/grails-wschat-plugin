85438612ae5108c021ff16c7dc0bfe2c53ff7a7d
Vahid <vahid.hedayati@gmail.com>
2014-10-05 21:20:43 +0100

    .

--- WschatGrailsPlugin.groovy
+++ WschatGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class WschatGrailsPlugin {
-    def version = "1.2"
+    def version = "1.3"
     def grailsVersion = "2.0 > *"
     def title = "Websocket Chat Plugin"
     def description = 'Default WebSocket Multi-chat room plugin, supports Admin privilages, kicking banning users. Webcam support for chrome/firefox. WebRTC (audio/video) support 0.24+'
