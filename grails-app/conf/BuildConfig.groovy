d95afe1940311f2a4fb817c5cadaa80c7e0bb5d0
vahid <vahid.hedayati@gmail.com>
2014-11-03 11:35:36 +0000

    updated plugins release/rest

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -22,7 +22,8 @@ grails.project.dependency.resolution = {
 			export = false
 		}
 		runtime ":pluginbuddy:0.3"
-		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+		
+		build ':release:3.0.1', ':rest-client-builder:2.0.3', {
 			export = false
 		}
 	}
