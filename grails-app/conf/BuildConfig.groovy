dc4cdca9fa244012987e5518233baf798b31672a
Vahid <vahid.hedayati@gmail.com>
2014-12-16 22:26:11 +0000

    could not publish plugin with that fancy hack :) haha

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,10 +1,8 @@
-import grails.util.Metadata
+
 
 grails.project.work.dir = 'target'
 
 grails.project.dependency.resolution = {
-	def appVersion=Metadata.current.'app.grails.version'
-	double appv=getGrailsVersion(appVersion)
 	inherits 'global'
 	log 'warn'
 
@@ -19,34 +17,16 @@ grails.project.dependency.resolution = {
 		//TicTacToe requirements
 		//compile 'org.codehaus.jackson:jackson-core-asl:1.9.8',		'org.codehaus.jackson:jackson-mapper-asl:1.9.8'
 	}
-	
-	if (appv>2.4) {
-		plugins {
-			build ":hibernate4:4.3.6.1", {
-				export = false
-			}
-			runtime ":pluginbuddy:0.3"
-			build ':release:3.0.1', ':rest-client-builder:2.0.3', {
-				export = false
-			}
-		}
 
-	}else if (appv<2.4) {
-		plugins {
-			build ":hibernate:3.6.10.6", {
-				export = false
-			}
-			runtime ":pluginbuddy:0.3"
-			build ':release:3.0.1', ':rest-client-builder:2.0.3', {
-				export = false
-			}
+	plugins {
+		build ":hibernate4:4.3.6.1", {
+			export = false
+		}
+		runtime ":pluginbuddy:0.3"
+		build ':release:3.0.1', ':rest-client-builder:2.0.3', {
+			export = false
 		}
 	}
-}
 
-private  getGrailsVersion(String appVersion) {
-	if (appVersion && appVersion.indexOf('.')>-1) {
-		int lastPos=appVersion.indexOf(".", appVersion.indexOf(".") + 1)
-		double verify=appVersion.substring(0,lastPos) as double
-	}
+
 }
