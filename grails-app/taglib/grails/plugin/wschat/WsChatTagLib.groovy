b7966c7cf94089300ca140ebd20eaf17f00c1059
Vahid Hedayati <badvad@gmail.com>
2015-07-04 18:52:19 +0100

    1.19 commented out junk removed

--- grails-app/taglib/grails/plugin/wschat/WsChatTagLib.groovy
+++ grails-app/taglib/grails/plugin/wschat/WsChatTagLib.groovy
@@ -56,59 +56,6 @@ class WsChatTagLib extends WsChatConfService {
 
 
 	def clientConnect  =  { attrs ->
-		/*
-		def room = attrs.remove('room')?.toString()
-		def actionMap = attrs.remove('actionMap')
-		def receivers = attrs.remove('receivers')
-		boolean strictMode = attrs.remove('strictMode')?.toBoolean() ?: false
-		boolean autodisco = attrs.remove('autodisco')?.toBoolean() ?: false
-		boolean masterNode = attrs.remove('masterNode')?.toBoolean() ?: false
-		boolean frontenduser = attrs.remove('frontenduser')?.toBoolean() ?: false
-		String hostname = attrs.remove('hostname')?.toString()
-		String appName = attrs.remove('appName')?.toString()
-		String user = attrs.remove('user')?.toString()
-		String message = attrs.remove('message')?.toString()
-		String divId = attrs.remove('divId')?.toString() ?: ''
-		String template = attrs.remove('template')?.toString()
-
-		if (receivers) {
-			receivers = receivers as ArrayList
-		}
-
-		String frontuser=''
-		if (frontenduser) {
-			frontuser=user+frontend
-			receivers.add(frontuser)
-		}
-
-		if (actionMap) {
-			actionMap = actionMap as Map
-		}
-
-		String dbSupport = config.dbsupport ?: 'yes'
-		String addAppName = config.add.appName ?: 'yes'
-		
-		if (!appName) {
-			appName = applicationName
-		}
-		
-		if (!hostname) {
-			hostname = config.hostname ?: 'localhost:8080'
-		}
-
-		if (!room) {
-			room = wsChatRoomService.returnRoom(dbSupport, true)
-		}
-
-		if (!message) {
-			message = "testing"
-		}
-
-		Map model = [  message : message, room: room, hostname: hostname, actionMap: actionMap,
-			appName: appName, frontuser:frontuser,  user: user, receivers: receivers, divId: divId,
-			chatApp: CHATAPP, addAppName: addAppName ]
-       */
-
 		def bean = new ClientTagBean(attrs)
 		if (!bean.validate()) {
 			bean.errors.allErrors.each {err ->
@@ -147,71 +94,6 @@ class WsChatTagLib extends WsChatConfService {
 	}
 
 	def clientWsConnect  =  { attrs ->
-		/*
-		def room = attrs.remove('room')?.toString()
-		def actionMap = attrs.remove('actionMap')
-		
-		def jsonData = attrs.remove('jsonData')
-		String sendType = attrs.remove('sendType')?.toString() ?: 'message'
-		String event =  attrs.remove('event')?.toString()
-		String context = attrs.remove('context')?.toString()
-		
-		def receivers = attrs.remove('receivers')
-		boolean strictMode = attrs.remove('strictMode')?.toBoolean() ?: false
-		boolean autodisco = attrs.remove('autodisco')?.toBoolean() ?: false
-		boolean masterNode = attrs.remove('masterNode')?.toBoolean() ?: false
-		boolean frontenduser = attrs.remove('frontenduser')?.toBoolean() ?: false
-		String hostname = attrs.remove('hostname')?.toString()
-		String appName = attrs.remove('appName')?.toString()
-		String user = attrs.remove('user')?.toString()
-		String message = attrs.remove('message')?.toString()
-		String divId = attrs.remove('divId')?.toString() ?: ''
-		String template = attrs.remove('template')?.toString()
-
-		String addAppName = config.add.appName ?: 'yes'
-		if (receivers) {
-			receivers = receivers as ArrayList
-		}
-		if (jsonData) {
-			if(jsonData instanceof String) {
-				jsonData =JSON.parse(jsonData)
-			}
-			jsonData = jsonData as JSON
-		}
-
-		String frontuser=''
-		if (frontenduser) {
-			frontuser=user+frontend
-			receivers.add(frontuser)
-		}
-
-		if (actionMap) {
-			actionMap = actionMap as Map
-		}
-		String dbSupport = config.dbsupport ?: 'yes'
-
-		if (!appName) {
-			appName = applicationName
-		}
-
-		if (!hostname) {
-			hostname = config.hostname ?: 'localhost:8080'
-		}
-
-		if (!room) {
-			room = wsChatRoomService.returnRoom(dbSupport,true)
-		}
-
-		if (!message) {
-			message = "testing"
-		}
-		
-		String uri="ws://${hostname}/${appName}/${CHATAPP}/"
-		if (addAppName=="no") {
-			uri="ws://${hostname}/${CHATAPP}/"
-		}
-		
-		*/
 		WsConnectTagBean bean = new WsConnectTagBean(attrs)
 		if (!bean.validate()) {
 			bean.errors.allErrors.each {err ->
@@ -223,11 +105,6 @@ class WsChatTagLib extends WsChatConfService {
 		}
 		Map model = [bean:bean]
 		Session oSession = chatClientListenerService.p_connect(bean.uri, bean.user, bean.room)
-		/*
-		Map model = [  message : message, room: room, hostname: hostname, actionMap: actionMap,
-			appName: appName, frontuser:frontuser,  user: user, receivers: receivers, divId: divId,
-			chatApp: CHATAPP, addAppName: addAppName ]
-		*/
 		try{
 			//closure(session)
 			if (bean.sendType == 'message') {
@@ -258,69 +135,6 @@ class WsChatTagLib extends WsChatConfService {
 	}
 
 	def complete = {attrs ->
-		/*
-		def clazz,name,cid,styles = ""
-		if (attrs.id == null) {
-			throwTagError("Tag [autoComplete] is missing required attribute [id]")
-		}
-		
-		if ( (!attrs.controller) && (!attrs.action) ) {
-			if (attrs.domain == null) {
-				throwTagError("Tag [autoComplete] is missing required attribute [domain]")
-			}
-			if (attrs.searchField == null) {
-				throwTagError("Tag [autoComplete] is missing required attribute [searchField]")
-			}
-		}
-		
-		if (!attrs.controller) {
-			attrs.controller= "wsChat"
-		}
-		if (!attrs.action) {
-			attrs.action= "autocomplete"
-		}
-		
-		if (!attrs.max) {
-			attrs.max = 10
-		}
-		if (!attrs.value) {
-			attrs.value =""
-		}
-		if (!attrs.order) {
-			attrs.order = "asc"
-		}
-		if (!attrs.collectField) {
-			attrs.collectField = attrs.searchField
-		}
-		
-			clazz = attrs.class
-		
-		if (attrs.style) {
-			styles = attrs.style
-		}
-		if (!attrs.name) {
-			name = ${attrs.id}
-		}else{
-		name=attrs.name
-		}
-	
-		def  required=""
-		Boolean requireField=true
-		if (attrs.require) {
-			requireField=attrs.remove('require')?.toBoolean()
-		}
-		
-		if (attrs.required) {
-			requireField=attrs.remove('required')?.toBoolean()
-		}
-		if (requireField) {
-			 required=" required='required' "
-		}
-
-		def template='/autoComplete/AutoCompleteBasic'
-		
-		def userTemplate=attrs.remove('userTemplate') ?: config.autocomplete
-		*/
 		AutoCompleteBean bean = new AutoCompleteBean(attrs)
 		if (!bean.validate()) {
 			bean.errors.allErrors.each {err ->
@@ -328,14 +142,10 @@ class WsChatTagLib extends WsChatConfService {
 			}
 		}
 		Map model = [bean:bean]
-		//def model = [attrs:attrs, clazz:clazz, styles:styles,name:name,required:required ]
-		 
 		if (bean.userTemplate) {
 			out << g.render(template:bean.userTemplate, model: model)
 		}else{
 			out << g.render(contextPath: pluginContextPath, template: bean.template, model:model)
 		}
-		
 	}
-	
 }
